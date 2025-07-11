6162c7c2f7128973d0eca5c288bed3ef0ad8a4a5
심명교 <gkdisdhfl1@naver.com>
2025-07-11 13:49:46 +0900

    value control widget 집중 모드 기능 추가

--- value_control_widget.cpp
+++ value_control_widget.cpp
@@ -1,11 +1,17 @@
 #include "value_control_widget.h"
 #include "ui_value_control_widget.h"
+#include <QDebug>
 
 ValueControlWidget::ValueControlWidget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::ValueControlWidget)
 {
     ui->setupUi(this);
+
+    // 초기 스텝 크기 설정
+    ui->valueSpinBox->setSingleStep(m_singleStep);
+    ui->valueSlider->setSingleStep(m_singleStep * m_multiplier);
+    ui->valueSlider->setPageStep(m_singleStep * m_multiplier * 10);
 
     // 슬라이더가 움직이면 onSliderValueChanged 슬롯 호출
     connect(ui->valueSlider, &QSlider::valueChanged, this, &ValueControlWidget::onSpinBoxValueChanged);
@@ -19,7 +25,7 @@ ValueControlWidget::~ValueControlWidget()
     delete ui;
 }
 
-// -- public 구현
+// --- public 구현 ---
 void ValueControlWidget::setRange(double min, double max)
 {
     ui->valueSpinBox->setRange(min, max);
@@ -39,7 +45,25 @@ double ValueControlWidget::value() const
     return ui->valueSpinBox->value();
 }
 
-// private 구현
+void ValueControlWidget::setSteps(double singleStep, double fineStep)
+{
+    m_singleStep = singleStep;
+    m_fineStep = fineStep;
+
+    // 새로운 기본 스텝으로 ui 업데이트
+    updateUiForTuningMode();
+}
+
+// event handler 구현
+void ValueControlWidget::mouseDoubleClickEvent(QMouseEvent *event)
+{
+    m_isFineTuningMode = !m_isFineTuningMode; // 상태 반전
+    updateUiForTuningMode();
+
+    QWidget::mouseDoubleClickEvent(event);
+}
+
+// --- private 구현 ---
 void ValueControlWidget::onSliderValueChanged(int value)
 {
     // 슬라이더 정수 값을 double로 변환
@@ -66,4 +90,28 @@ void ValueControlWidget::onSpinBoxValueChanged(double value)
     ui->valueSlider->setValue(intValue);
 
     emit valueChanged(value);
+}
+
+void ValueControlWidget::updateUiForTuningMode()
+{
+    if(m_isFineTuningMode) {
+        qDebug() << "Fine-tuning mode: ON";
+
+        // 집중 모드일 때 step을 fineStep으로 변경
+        ui->valueSpinBox->setSingleStep(m_fineStep);
+        ui->valueSlider->setSingleStep(m_fineStep * m_multiplier);
+        ui->valueSlider->setPageStep(m_fineStep * m_multiplier * 10);
+
+        // 배경색 변경
+        ui->valueSpinBox->setStyleSheet("background-color: #e0f0ff;");
+    } else {
+        // 일반모드 일 때 step을 singleStep으로 변경
+        qDebug() << "Fine-tuning mode: OFF";
+        ui->valueSpinBox->setSingleStep(m_singleStep);
+        ui->valueSlider->setSingleStep(m_singleStep * m_multiplier);
+        ui->valueSlider->setPageStep(m_singleStep * m_multiplier * 10);
+
+        // 배경색 변경
+        ui->valueSpinBox->setStyleSheet("");
+    }
 }
--- value_control_widget.h
+++ value_control_widget.h
@@ -17,6 +17,7 @@ public:
 
     void setRange(double min, double max);
     void setValue(double value);
+    void setSteps(double singleStep, double fineStep);
     double value() const; // 현재 값을 가져오는 함수
 
 protected:
@@ -33,8 +34,16 @@ signals:
 private:
     Ui::ValueControlWidget *ui;
 
+    // UI를 현재 모드에 맞게 업데이트하는 내부 함수
+    void updateUiForTuningMode();
+
     // 스케일 조정을 위한 변수
     double m_multiplier = 100.0;
+
+    // 집중 모드 관련 멤버 변수들
+    bool m_isFineTuningMode = false;
+    double m_singleStep = 1.0;
+    double m_fineStep = 0.1;
 };
 
 #endif // VALUE_CONTROL_WIDGET_H
