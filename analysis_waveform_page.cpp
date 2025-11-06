d09d94e0371d5c2cb9cb45d8b14ccbf2628bd30d
심명교 <gkdisdhfl1@naver.com>
2025-11-06 09:53:31 +0900

    Feat(ui): AnalysisWaveformPage UI 재구현
    `AnalysisWaveformPage`를 `BaseGraphWindow` 상속에서 `QWidget` 직접 상속으로 변경하고,
    UI 요구사항에 맞춰 재구현함.
    
    주요 변경 사항:
      - `AnalysisWaveformPage`의 상속 구조를 `QWidget`으로 변경하고, `setupUi()` 함수를 통해
    모든 UI 요소를 직접 구성하도록 함.
    
      - 상단 제목, 전체 Voltage/Current 체크박스, 구분선, 중앙 컨트롤바, 왼쪽 스케일 버튼 그룹, 3상 전압/
    전류를 위한 차트 뷰를 포함하는 복합 레이아웃을 구현함.
    
      - 차트의 X축은 숨기고, Y축은 왼쪽(전압)과 오른쪽(전류)으로 분리하여 설정함. 각 Y축은 중앙 0을 기준으로 8칸으로
    나뉘도록 설정함.

--- analysis_waveform_page.cpp
+++ analysis_waveform_page.cpp
@@ -1,71 +1,225 @@
 #include "analysis_waveform_page.h"
-#include "custom_chart_view.h"
+#include <QButtonGroup>
 #include <QChart>
+#include <QChartView>
+#include <QCheckBox>
+#include <QLabel>
 #include <QLineSeries>
+#include <QPushButton>
+#include <QVBoxLayout>
 #include <QValueAxis>
 
 AnalysisWaveformPage::AnalysisWaveformPage(QWidget* parent)
-    : BaseGraphWindow(nullptr, parent)
+    : QWidget(parent)
+    , m_isUpdating(true)
 {
-    setupSeries();
+    setupUi();
 }
 
-void AnalysisWaveformPage::setupSeries()
+void AnalysisWaveformPage::setupUi()
 {
-    m_chartView->setInteractive(false); // 스크롤, 줌 금지
-    m_chart->legend()->hide(); // 범례 숨기기
-    toggleAutoScroll(false); // 자동 스크롤 끄기
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setContentsMargins(15, 10, 15, 10);
+    mainLayout->setSpacing(5);
 
-    m_axisY = new QValueAxis(this);
-    m_axisY->setTitleText("V/A");
-    m_chart->addAxis(m_axisY, Qt::AlignLeft);
+    // 1. 상단 UI (제목, 전체 on/off)
+    auto topLayout = new QHBoxLayout();
+    QLabel* titleLabel = new QLabel("Waveform");
+    titleLabel->setObjectName("titleLabel");
+    topLayout->addWidget(titleLabel);
+    topLayout->addStretch();
 
-    // A상 전압/전류 시리즈
-    m_voltageSeries = new QLineSeries(this);
-    m_currentSeries = new QLineSeries(this);
-    m_voltageSeries->setColor(QColor("blue"));
-    m_currentSeries->setColor(QColor("red"));
+    auto* voltageAllCheck = new QCheckBox("Voltage");
+    auto* currentAllCheck = new QCheckBox("Current");
+    voltageAllCheck->setChecked(true);
+    currentAllCheck->setChecked(true);
+    voltageAllCheck->setObjectName("check");
+    currentAllCheck->setObjectName("check");
+    topLayout->addWidget(voltageAllCheck);
+    topLayout->addWidget(currentAllCheck);
+    mainLayout->addLayout(topLayout);
 
-    m_chart->addSeries(m_voltageSeries);
-    m_chart->addSeries(m_currentSeries);
-    m_voltageSeries->attachAxis(m_axisX);
-    m_voltageSeries->attachAxis(m_axisY);
-    m_currentSeries->attachAxis(m_axisX);
-    m_currentSeries->attachAxis(m_axisY);
-};
+    // 2. 구분선
+    QFrame* titleLine = new QFrame(this);
+    titleLine->setFrameShape(QFrame::HLine);
+    titleLine->setObjectName("titleLine");
+    titleLine->setFixedHeight(2);
+    mainLayout->addWidget(titleLine);
+
+    // 3. 중간 컨트롤 바 (시작/정지, 상별 on/off)
+    auto controlBarLayout = new QHBoxLayout();
+    m_startButton = new QPushButton("❚❚");
+    m_startButton->setCheckable(true);
+    m_startButton->setChecked(true);
+    controlBarLayout->addWidget(m_startButton);
+    controlBarLayout->addSpacing(20);
+
+    QLabel* voltageLabel = new QLabel("Volt");
+    QLabel* currentLabel = new QLabel("Curr");
+    voltageLabel->setObjectName("waveformLabel");
+    currentLabel->setObjectName("waveformLabel");
+
+    controlBarLayout->addWidget(voltageLabel);
+    for(int i{0}; i < 3; ++i) {
+        m_voltagePhaseChecks[i] = new QCheckBox(QString(QChar('A' + i)));
+        m_voltagePhaseChecks[i]->setChecked(true);
+        controlBarLayout->addWidget(m_voltagePhaseChecks[i]);
+    }
+    controlBarLayout->addSpacing(20);
+
+    controlBarLayout->addWidget(currentLabel);
+    for(int i{0}; i < 3; ++i) {
+        m_currentPhaseChecks[i] = new QCheckBox(QString(QChar('A' + i)));
+        m_currentPhaseChecks[i]->setChecked(true);
+        controlBarLayout->addWidget(m_currentPhaseChecks[i]);
+    }
+    controlBarLayout->addStretch();
+    mainLayout->addLayout(controlBarLayout);
+
+    // 4. 컨텐츠 영역 (스케일 버튼, 차트)
+    auto contentLayout = new QHBoxLayout();
+
+    // 4-1 스케일 버튼 그룹
+    auto scaleButtonsContainer = new QWidget();
+    scaleButtonsContainer->setObjectName("scaleButtonsContainer");
+    scaleButtonsContainer->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Preferred);
+    auto scaleButtonsLayout = new QVBoxLayout(scaleButtonsContainer);
+    // scaleButtonsLayout->setSpacing(10);
+    // scaleButtonsLayout->setContentsMargins(5, 5, 5, 5);
+
+    m_scaleButtonGroup = new QButtonGroup(this);
+    m_scaleButtonGroup->setExclusive(true);
+    for(int i{0}; i < 4; ++i) {
+        m_scaleButtons[i] = new QPushButton(QString("x%1").arg(i + 1));
+        m_scaleButtons[i]->setCheckable(true);
+        m_scaleButtons[i]->setObjectName("waveformScaleButton");
+        scaleButtonsLayout->addWidget(m_scaleButtons[i]);
+        m_scaleButtonGroup->addButton(m_scaleButtons[i], 1);
+    }
+    m_scaleButtons[0]->setChecked(true);
+    scaleButtonsLayout->addStretch();
+    contentLayout->addWidget(scaleButtonsContainer);
+
+    // 4-2 차트 뷰
+    m_chart = new QChart();
+    m_chartView = new QChartView(m_chart);
+    m_chartView->setRenderHint(QPainter::Antialiasing);
+    contentLayout->addWidget(m_chartView, 1);
+
+    // 차트 및 축 설정
+    m_chart->legend()->hide();
+    m_chart->setContentsMargins(-10, -10, -10, -10); // 여백 최소화
+
+    m_axisX = new QValueAxis();
+    m_axisX->setLabelsVisible(false);
+    m_axisX->setTickCount(11);
+    m_chart->addAxis(m_axisX, Qt::AlignBottom);
+
+    m_axisV = new QValueAxis();
+    m_axisV->setTitleText("V");
+    m_axisV->setRange(-400, 400);
+    m_axisV->setTickCount(9);
+    m_axisV->setLabelFormat("%.0f");
+    m_chart->addAxis(m_axisV, Qt::AlignLeft);
+
+    m_axisA = new QValueAxis();
+    m_axisA->setTitleText("A");
+    m_axisA->setRange(-10, 10);
+    m_axisA->setTickCount(9);
+    m_axisA->setLabelFormat("%.1f");
+    m_chart->addAxis(m_axisA, Qt::AlignRight);
+
+    // 시리즈 생성 및 연결
+    const std::array<QColor, 3> voltageColors = {Qt::blue, Qt::darkYellow, Qt::black};
+    const std::array<QColor, 3> currentColors = {Qt::red, QColorConstants::Svg::orange, QColorConstants::Svg::gray};
+
+    mainLayout->addLayout(contentLayout, 1);
+
+    for(int i{0}; i < 3; ++i) {
+        m_voltageSeries[i] = new QLineSeries();
+        m_voltageSeries[i]->setColor(voltageColors[i]);
+        m_chart->addSeries(m_voltageSeries[i]);
+        m_voltageSeries[i]->attachAxis(m_axisX);
+        m_voltageSeries[i]->attachAxis(m_axisV);
+        connect(m_voltagePhaseChecks[i], &QCheckBox::toggled, m_voltageSeries[i], &QLineSeries::setVisible);
+
+        m_currentSeries[i] = new QLineSeries();
+        m_currentSeries[i]->setColor(currentColors[i]);
+        m_chart->addSeries(m_currentSeries[i]);
+        m_currentSeries[i]->attachAxis(m_axisX);
+        m_currentSeries[i]->attachAxis(m_axisA);
+        connect(m_currentPhaseChecks[i], &QCheckBox::toggled, m_currentSeries[i], &QLineSeries::setVisible);
+    }
+
+    // 전체 on/off 체크박스 연결
+    connect(voltageAllCheck, &QCheckBox::toggled, [this](bool checked){
+        for(int i{0}; i < 3; ++i) m_voltagePhaseChecks[i]->setChecked(checked);
+    });
+    connect(currentAllCheck, &QCheckBox::toggled, [this](bool checked){
+        for(int i{0}; i < 3; ++i) m_currentPhaseChecks[i]->setChecked(checked);
+    });
+
+    // 시작/정지 버튼 연결
+    connect(m_startButton, &QPushButton::toggled, this, [this](bool checked){
+        m_isUpdating = checked;
+        m_startButton->setText(checked ? "❚❚" : "▶");
+    });
+}
 
 void AnalysisWaveformPage::updateWaveformData(const OneSecondSummaryData& data)
 {
+    if(!m_isUpdating || data.lastTwoCycleData.empty())
+        return;
+
     const auto& waveData = data.lastTwoCycleData;
-    if(waveData.empty()) return;
+
+    // 데이터 포인트들을 각 시리즈에 맞게 분리
 
     // 데이터의 마지막 N개(2 사이클 분량)를 가져옴
-    QList<QPointF> vPoints, iPoints;
-    vPoints.reserve(waveData.size());
-    iPoints.reserve(waveData.size());
+    std::array<QList<QPointF>, 3> vPoints, iPoints;
+    for(int i{0}; i < 3; ++i) {
+        vPoints[i].reserve(waveData.size());
+        iPoints[i].reserve(waveData.size());
+    }
 
-    double minY = std::numeric_limits<double>::max();
-    double maxY = std::numeric_limits<double>::lowest();
+    double minV = std::numeric_limits<double>::max();
+    double maxV = std::numeric_limits<double>::lowest();
+    double minA = std::numeric_limits<double>::max();
+    double maxA = std::numeric_limits<double>::lowest();
 
     for (const auto& point : waveData) {
         double timeSec = std::chrono::duration<double>(point.timestamp).count();
-        double v = point.voltage.a;
-        double i = point.current.a;
+        const auto& v = point.voltage;
+        const auto& i = point.current;
 
-        vPoints.append(QPointF(timeSec, v));
-        iPoints.append(QPointF(timeSec, i));
+        vPoints[0].append(QPointF(timeSec, v.a));
+        vPoints[1].append(QPointF(timeSec, v.b));
+        vPoints[2].append(QPointF(timeSec, v.c));
 
-        minY = std::min({minY, v, i});
-        maxY = std::max({maxY, v, i});
+        iPoints[0].append(QPointF(timeSec, i.a));
+        iPoints[1].append(QPointF(timeSec, i.b));
+        iPoints[2].append(QPointF(timeSec, i.c));
+
+        minV = std::min({minV, v.a, v.b, v.c});
+        maxV = std::max({maxV, v.a, v.b, v.c});
+        minA = std::min({minA, i.a, i.b, i.c});
+        maxA = std::max({maxA, i.a, i.b, i.c});
     }
 
     // 시리즈 및 축 업데이트
-    m_voltageSeries->replace(vPoints);
-    m_currentSeries->replace(iPoints);
+    for(int i{0}; i < 3; ++i) {
+        m_voltageSeries[i]->replace(vPoints[i]);
+        m_currentSeries[i]->replace(iPoints[i]);
+    }
 
-    if (!vPoints.isEmpty()) {
-        m_axisX->setRange(vPoints.first().x(), vPoints.last().x());
-        double padding = (maxY - minY) * 0.1 + 1.0;
-        m_axisY->setRange(minY - padding, maxY + padding);
+    // 축 범위 설정
+    if (!waveData.empty()) {
+        m_axisX->setRange(vPoints[0].first().x(), vPoints[0].last().x());
+
+        double voltagePadding = (maxV - minV) * 0.1 + 1.0;
+        m_axisV->setRange(minV - voltagePadding, maxV + voltagePadding);
+
+        double amperePadding = (maxA - minA) * 0.1 + 1.0;
+        m_axisA->setRange(minA - amperePadding, maxA + amperePadding);
     }
 }
--- analysis_waveform_page.h
+++ analysis_waveform_page.h
@@ -1,25 +1,50 @@
 #ifndef ANALYSIS_WAVEFORM_PAGE_H
 #define ANALYSIS_WAVEFORM_PAGE_H
 
-#include "base_graph_window.h"
 #include "measured_data.h"
 
 #include <QWidget>
 
-class AnalysisWaveformPage : public BaseGraphWindow
+class QChart;
+class QChartView;
+class QLineSeries;
+class QValueAxis;
+
+class QPushButton;
+class QCheckBox;
+class QButtonGroup;
+
+class AnalysisWaveformPage : public QWidget
 {
     Q_OBJECT
 public:
     explicit AnalysisWaveformPage(QWidget *parent = nullptr);
-    void setupSeries() override;
 
 public slots:
     void updateWaveformData(const OneSecondSummaryData& data);
 
 private:
-    QLineSeries* m_voltageSeries;
-    QLineSeries* m_currentSeries;
-    QValueAxis* m_axisY;
+    void setupUi();
+
+    // UI 위젯
+    QPushButton* m_startButton;
+    std::array<QCheckBox*, 3> m_voltagePhaseChecks;
+    std::array<QCheckBox*, 3> m_currentPhaseChecks;
+    std::array<QPushButton*, 4> m_scaleButtons;
+    QButtonGroup* m_scaleButtonGroup;
+
+    // 차트 관련
+    QChart* m_chart;
+    QChartView* m_chartView;
+    QValueAxis* m_axisX;
+    QValueAxis* m_axisV;
+    QValueAxis* m_axisA;
+
+    // 3상 전압/전류 시리즈
+    std::array<QLineSeries*, 3> m_voltageSeries;
+    std::array<QLineSeries*, 3> m_currentSeries;
+
+    bool m_isUpdating; // 시작/정지 상태
 };
 
 #endif // ANALYSIS_WAVEFORM_PAGE_H
